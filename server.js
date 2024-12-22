d07b64da41be1ca9da2687723df92a3b77b286d5
Tony Liu <tliu55@myseneca.ca>
2024-12-22 15:18:26 -0500

    reorganized files, implemented ejs, installed express

---
+++ server.js
@@ -0,0 +1,28 @@
+//boilerplate ejs setup
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+    res.render('index');
+});
+
+app.get('/projects', (req,res)=>{
+    res.render('projects');
+})
+
+app.get('/resume', (req, res) => {
+    res.render('resume');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});

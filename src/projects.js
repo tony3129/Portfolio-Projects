c41d3469a2016c4dc11d62fe4bc92ddfb51fdbe8
Tony Liu <tliu55@myseneca.ca>
2025-01-04 15:52:17 -0500

    to store data to populate projects.ejs page

---
+++ src/projects.js
@@ -0,0 +1,18 @@
+const projects = [
+    {
+        title: 'Snake Game',
+        desc: 'HTML, CSS, JavaScript (Desktop Only)',
+        img: './assets/snakeLogo.webp',
+        link: '/snakeGame',
+        alt: 'Snake Game Logo'
+    },
+    {
+        title: 'Weather App',
+        desc: 'HTML, CSS, JavaScript, OpenWeather API',
+        img: './assets/weatherLogo.webp',
+        link: '/weatherApp',
+        alt: 'Weather App Logo'
+    }
+]
+
+module.exports = projects;

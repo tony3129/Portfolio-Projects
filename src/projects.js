1d76fef651b876b30d8a1f59afed07efb1a64826
Tony Liu <tliu55@myseneca.ca>
2025-01-06 16:16:35 -0500

    added icon and card for wishlist app

--- src/projects.js
+++ src/projects.js
@@ -2,6 +2,7 @@ const projects = [
     {
         title: 'Snake Game',
         desc: 'HTML, CSS, JavaScript (Desktop Only)',
+        //taken from: https://wallpapers.com/wallpapers/green-snake-game-character-bqvxt2p5oibadgqn.html
         img: './assets/snakeLogo.webp',
         link: '/snakeGame',
         alt: 'Snake Game Logo'
@@ -9,9 +10,18 @@ const projects = [
     {
         title: 'Weather App',
         desc: 'HTML, CSS, JavaScript, OpenWeather API',
+        //taken from: https://www.iconfinder.com/icons/4102326/cloud_sun_sunny_weather_icon
         img: './assets/weatherLogo.webp',
         link: '/weatherApp',
         alt: 'Weather App Logo'
+    },
+    {
+        title: 'Wishlist App',
+        desc: 'HTML, CSS, JavaScript, bcrypt, client-sessions, PostgreSQL, JQuery',
+        //taken from: https://www.flaticon.com/free-icon/wish-list_776611
+        img: './assets/wishListLogo.png',
+        link: '/wishlist',
+        alt: 'Wishlist App Logo'
     }
 ]
 

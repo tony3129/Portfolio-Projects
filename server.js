92541882f04e5038259c20753ffbeb1318957e5f
Tony Liu <tliu55@myseneca.ca>
2025-01-05 20:53:50 -0500

    rearranged code

--- server.js
+++ server.js
@@ -197,10 +197,6 @@ app.post('/wishlist/add', async (req,res)=>{
     }
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
-
 //allow deletion of items
 app.post('/wishlist/delete/:id', async (req,res)=>{
     //delete based on itemID
@@ -217,3 +213,7 @@ app.post('/wishlist/delete/:id', async (req,res)=>{
         res.redirect('/wishlist');
     }
 })
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});

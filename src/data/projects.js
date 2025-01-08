8f06333038d3f36d2ff62a2da22f0015a580a53d
Tony Liu <tliu55@myseneca.ca>
2025-01-08 16:51:33 -0500

    reorganized file structure, added more key:value pairs

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,9 @@ const projects = [
         desc: 'HTML, CSS, JavaScript (Desktop Only)',
         //taken from: https://wallpapers.com/wallpapers/green-snake-game-character-bqvxt2p5oibadgqn.html
         img: './assets/snakeLogo.webp',
+        live: true,
         link: '/snakeGame',
+        github: 'https://github.com/tony3129/Snake',
         alt: 'Snake Game Logo'
     },
     {
@@ -12,7 +14,9 @@ const projects = [
         desc: 'HTML, CSS, JavaScript, OpenWeather API',
         //taken from: https://www.iconfinder.com/icons/4102326/cloud_sun_sunny_weather_icon
         img: './assets/weatherLogo.webp',
+        live: true,
         link: '/weatherApp',
+        github: 'https://github.com/tony3129/Weather',
         alt: 'Weather App Logo'
     },
     {
@@ -20,7 +24,9 @@ const projects = [
         desc: 'HTML, CSS, JavaScript, bcrypt, client-sessions, PostgreSQL, JQuery',
         //taken from: https://www.flaticon.com/free-icon/wish-list_776611
         img: './assets/wishListLogo.png',
+        live: true,
         link: '/wishlist',
+        github: 'https://github.com/tony3129/Wishlist',
         alt: 'Wishlist App Logo'
     }
 ]

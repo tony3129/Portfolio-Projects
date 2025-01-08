6bc5658aad29c1cad8413a4eda3cd9f688c44f5d
Tony Liu <tliu55@myseneca.ca>
2025-01-08 10:22:26 -0500

    added console log for debugging

--- middleware/wishListLogin.js
+++ middleware/wishListLogin.js
@@ -1,5 +1,6 @@
 function requireLogin(req, res, next) {
     if(!req.session.user){
+        console.log('Login failed: invalid credentials')
         return res.redirect('/login');
     }
     next();
